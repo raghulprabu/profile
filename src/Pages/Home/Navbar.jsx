822efc34e8feba0295d02776133a4945a90b1fdd
raghulbbnl-lg <raghulbbnl@gmail.com>
2026-05-11 15:14:12 +0530

    navbar message

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth <= 2200) {
+      if (window.innerWidth <= 1200) {
         closeMenu();
       }
     };
@@ -27,90 +27,109 @@ function Navbar() {
   }, []);
 
   return (
-    <nav className={`navbar ${navActive ? "active" : ""}`}>
-      <div>
-        <img src="img/logo.png" alt="RAGHUL" />
+    <nav className="navbar-modern">
+      <div className="navbar-modern__container">
+        <div
+          className={`nav__hamburger ${navActive ? "active" : ""}`}
+          onClick={toggleNav}
+        >
+          <span className="nav__hamburger__line"></span>
+          <span className="nav__hamburger__line"></span>
+          <span className="nav__hamburger__line"></span>
+        </div>
+
+        <div className={`navbar-modern__content ${navActive ? "active" : ""}`}>
+          <ul className="navbar-modern__list">
+            <li className="navbar-modern__item">
+              <Link
+                onClick={closeMenu}
+                activeClass="navbar-modern__link--active"
+                spy={true}
+                smooth={true}
+                offset={-90}
+                duration={500}
+                to="heroSection"
+                className="navbar-modern__link"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="navbar-modern__item">
+              <Link
+                onClick={closeMenu}
+                activeClass="navbar-modern__link--active"
+                spy={true}
+                smooth={true}
+                offset={-90}
+                duration={500}
+                to="AboutMe"
+                className="navbar-modern__link"
+              >
+                About
+              </Link>
+            </li>
+            <li className="navbar-modern__item">
+              <Link
+                onClick={closeMenu}
+                activeClass="navbar-modern__link--active"
+                spy={true}
+                smooth={true}
+                offset={-90}
+                duration={500}
+                to="MySkills"
+                className="navbar-modern__link"
+              >
+                Service
+              </Link>
+            </li>
+
+            <li className="navbar-modern__brand">RAGHUL</li>
+
+            <li className="navbar-modern__item">
+              <Link
+                onClick={closeMenu}
+                activeClass="navbar-modern__link--active"
+                spy={true}
+                smooth={true}
+                offset={-90}
+                duration={500}
+                to="MyPortfolio"
+                className="navbar-modern__link"
+              >
+                Resume
+              </Link>
+            </li>
+            <li className="navbar-modern__item">
+              <Link
+                onClick={closeMenu}
+                activeClass="navbar-modern__link--active"
+                spy={true}
+                smooth={true}
+                offset={-90}
+                duration={500}
+                to="MyPortfolio"
+                className="navbar-modern__link"
+              >
+                Project
+              </Link>
+            </li>
+            <li className="navbar-modern__item">
+              <Link
+                onClick={closeMenu}
+                activeClass="navbar-modern__link--active"
+                spy={true}
+                smooth={true}
+                offset={-90}
+                duration={500}
+                to="Contact"
+                className="navbar-modern__link"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div
-        className={`nav__hamburger ${navActive ? "active" : ""}`}
-        onClick={toggleNav}
-      >
-        <span className="nav__hamburger__line"></span>
-        <span className="nav__hamburger__line"></span>
-        <span className="nav__hamburger__line"></span>
-      </div>
-      <div className={`navbar--items ${navActive ? "active" : ""}`}>
-        <ul>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar--active-content"
-              spy={true}
-              smooth={true}
-              offset={-90}
-              duration={500}
-              to="heroSection"
-              className="navbar--content"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar--active-content"
-              spy={true}
-              smooth={true}
-              offset={-90}
-              duration={500}
-              to="MyPortfolio"
-              className="navbar--content"
-            >
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar--active-content"
-              spy={true}
-              smooth={true}
-              offset={-90}
-              duration={500}
-              to="AboutMe"
-              className="navbar--content"
-            >
-              About Me
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar--active-content"
-              spy={true}
-              smooth={true}
-              offset={-90}
-              duration={500}
-              to="testimonial"
-              className="navbar--content"
-            >
-              Projects Ratings
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <Link
-        onClick={closeMenu}
-        activeClass="navbar--active-content"
-        spy={true}
-        smooth={true}
-        offset={-90}
-        duration={500}
-        to="Contact"
-        className="btn btn-outline-primary"
-      >
-        Contact Me
-      </Link>
     </nav>
   );
 }
